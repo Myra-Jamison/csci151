efdb2a730f86a9da7d0c8bba45b6431afb0a9e55
Myra-Jamison <myrajamison@protonmail.com>
2024-02-08 12:22:57 -0700

    dice mostly finished

--- dice.py
+++ dice.py
@@ -1,17 +1,20 @@
 #----------------
-# Boys and Girls Assignment
+# Dice assignment
 # ----------------
-# Myra Jamison, Jan 31, CSCI151
+# Myra Jamison, Feb 8th, CSCI151
 
 import stdio
 import sys
 import stdarray
 import random
 
+#description: simulate the rolling of two 6-sided dice and the sum produced for n trials
 
 #create empty array for the results, gather user input
 diceResults = stdarray.create1D(12, value=0)
 trials = int(sys.argv[1])
+probabilitiesPercent = stdarray.create1D(11, value=0.0)
+
 
 #provided script describing the probabilities of two dice
 probabilities = stdarray.create1D(13, 0.0)
@@ -25,13 +28,17 @@ for k in range(trials):
     roll = random.random()
     count = 0
     percentTracker = list()
-    for j in probabilitiesPercent[2:13]:
+    for l in probabilitiesPercent[2:13]:
         count += 1
-        percentTracker.append(j)
+        percentTracker.append(l)
         if roll < sum(percentTracker):
             diceResults[count] += 1
             break
 
 stdio.write(diceResults)
 
+#confused about how lists are mutable vs immutable arrays produced by the booksite module... can
+# we iterate over the booksite arrays?
 
+#also, its a lot; to get our theoretical and experimental to match to 3 decimal places,
+# approximately 10^6 trials are required
