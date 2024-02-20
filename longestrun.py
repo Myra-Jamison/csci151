6c5bb89c8456bc1c3e033395ce7e06e7ceb6ff43
Myra Jamison <myrajamison@protonmail.com>
2024-02-19 21:10:12 -0700

    longest run upload

--- longestrun.py
+++ longestrun.py
@@ -1,7 +1,28 @@
-import sys
-import stdarray
-import stdio
-import random
+#----------------
+# Longest Run
+# ----------------
+# Myra Jamison, February 19, CSCI151
 
-sequence = random.randrange(1,10,1)
-stdio.readInt()
+#read a sequence of integers from standard input, then calculate the longest
+#consecutive run of the same integer
+
+import stdio
+import stdarray
+
+#read stdin
+input = list(stdio.readAllInts())
+
+#make an array to store results in, then calculate consecutive runs
+storedResults = stdarray.create1D(max(input),1)
+count = 0
+for i in input:
+    if input[count] == input[count-1] and count > 0:
+        storedResults[i] += 1
+    count += 1
+
+#find value and length of longest consecutive run
+length = max(storedResults)
+value = storedResults.index(length)
+
+#write results to stdout
+stdio.write('Longest Run: ' + str(length) + ' consecutive ' + str(value) +'s')
