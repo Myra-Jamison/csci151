43f398f8b5a89b54ae74805ab27082292049108a
Myra-Jamison <myrajamison@protonmail.com>
2024-04-14 14:04:13 -0600

    im1done

--- im1.py
+++ im1.py
@@ -1,3 +1,29 @@
-import grayscale
+import stddraw
+import pygame
+import stdarray
+
+pic = pygame.image.load('/home/myra/Documents/csci151/mandrill.jpg')
+
+listOfIntensities = list()
+frequencyIntensity = stdarray.create1D(256, 0)
+
+for x in range(pic.get_width()):
+    for y in range(pic.get_height()):
+        colorPixel = pic.get_at((x,y))
+        grayPixel = tuple(colorPixel.grayscale())
+        print(grayPixel)
+        listOfIntensities.append(grayPixel[0])
+
+count = 0
+for i in listOfIntensities:
+    frequencyIntensity[i] += 1
+print(frequencyIntensity)
+
+stddraw.setYscale(0,1200)
+stddraw.setXscale(0,270)
+for j in frequencyIntensity:
+    stddraw.filledRectangle(count,1,0.9,j)
+    count += 1
 
 
+stddraw.show()
